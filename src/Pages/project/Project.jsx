d4f49d533cfb148f9b22d6fa86d9591b5660409e
Omor Faruk <omor3710@gmail.com>
2024-01-25 12:08:54 +0600

    add project  and design

--- src/Pages/project/Project.jsx
+++ src/Pages/project/Project.jsx
@@ -2,31 +2,28 @@ import data from "../../data/index.json";
 import "./project.css"
 export default function Project() {
   return (
-    <section className="portfolio--section" id="MyPortfolio">
-      <div className="portfolio--container-box">
-        <div className="portfolio--container">
-          <h2 className="section--heading">My Project</h2>
-        </div>
-        <div>
-          <button className="btn btn-github">
-            Visit My GitHub
-          </button>
-        </div>
+    <section className="project--section" id="Myproject">
+      <div className="project--container-box">
+        <h1 className="section--heading">My Project</h1>
       </div>
-      <div className="portfolio--section--container">
-        {data?.portfolio?.map((item, index) => (
-          <div key={index} className="portfolio--section--card">
-            <div className="portfolio--section--img">
+      <div className="project--section--container">
+        {data?.project?.map((item, index) => (
+          <div key={index} className="project--section--card">
+            <div className="project--section--img">
               <img src={item.src} alt="Placeholder" />
             </div>
-            <div className="portfolio--section--card--content">
+            <div className="project--section--card--content">
               <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
+                <h3 className="project--section--title">{item.title}</h3>
               </div>
-              <p className="text-sm portfolio--link">
-                <a target="_blank" href={item.link}>{item.link}</a>
-              </p>
+              <div className="project-link">
+                <span>
+                  <a href={item.link1} target="_blank">Github </a>
+                </span>
+                <span>
+                  <a href={item.link2} target="_blank">Live link </a>
+                </span>
+              </div>
             </div>
           </div>
         ))}
--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -15,7 +15,9 @@ export default function HeroSection() {
             <br /> Dolorum, quas. Amet soluta assumenda cum?
           </p>
         </div>
-        <button className="btn btn-primary">Download Resume</button>
+        <button className="btn btn-primary">
+          <a href="https://drive.google.com/file/d/1Pe03YkIDERVh9jmJZh46VRCMNrRdJebi/view?usp=sharing" target="_blank">Download Resume</a>
+        </button>
       </div>
       <div className="hero-img">
         <img className="hero-img" src="./img/hero-img.jpg" alt="Hero Section" />
