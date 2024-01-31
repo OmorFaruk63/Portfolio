37929e974e2cb57bf79f1a4423e4207e25d5077a
Omor Faruk <omor3710@gmail.com>
2024-01-31 17:13:18 +0600

    add about

--- src/Pages/about/AboutMe.jsx
+++ src/Pages/about/AboutMe.jsx
@@ -1,28 +1,32 @@
-import "./about.css"
+import "./about.css";
 export default function AboutMe() {
   return (
-
-    <section id="AboutMe" className="about--section">
-      <div className="about--section--img">
-        <img src="./img/mainLogo.jpg" alt="About Me" />
-      </div>
-      <div className="hero--section--content--box about--section--box">
-        <div className="hero--section--content">
-          <p className="section--title">About</p>
-          <h1 className="skills-section--heading">About Me</h1>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
-          </p>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
-          </p>
-        </div>
-      </div>
+    <section className="about-section">
+      <p className="about-title">
+        Madrasa to Front-end Development: My Journey
+      </p>
+      <p className="about-content">
+        Hello, I am Omor Faruk. I received my education in a Madrasa, where
+        emphasis was placed on Islamic education. Despite my academic focus on
+        religious studies, my passion for programming was immense. After
+        completing my studies in the Madrasa, my interest in programming led me
+        to start learning online.
+      </p>
+      <p className="about-content">
+        I delved into documentation and watched tutorials on YouTube to
+        kickstart my programming journey. I started with HTML, CSS, and
+        JavaScript. As I progressed, my curiosity and love for programming grew.
+        Now, I proudly identify myself as a Front-end Developer.
+      </p>
+      <p className="about-content">
+        My current skill set includes expertise in HTML, CSS, JavaScript, and
+        React. This journey, transitioning from a Madrasa background to becoming
+        a Front-end Developer, is a testament to my dedication and enthusiasm
+        for programming.
+      </p>
+      <p className="about-content">
+        Stay tuned for more updates on my coding journey and projects!
+      </p>
     </section>
   );
 }
